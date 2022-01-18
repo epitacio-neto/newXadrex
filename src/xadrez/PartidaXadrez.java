5b73692136015843129bc27a7f9d5937ae31e84d
epitacio-neto <epitacio.neto.92@gmail.com>
2022-01-17 22:44:24 -0300

    tabuleiro e peças

---
+++ src/xadrez/PartidaXadrez.java
@@ -0,0 +1,16 @@
+package xadrez;
+
+import tabuleiro.Tabuleiro;
+
+public class PartidaXadrez {
+	
+	private Tabuleiro tabuleiro;
+	
+	public PartidaXadrez() {
+		tabuleiro = new Tabuleiro(8,8);
+	}
+	
+	public PecaXadrez[][] getPecas(){
+		PecaXadrez[][] mat = new PecaXadrez[tabuleiro.getLinhas()][tabuleiro.getColunas()];
+	}
+}
